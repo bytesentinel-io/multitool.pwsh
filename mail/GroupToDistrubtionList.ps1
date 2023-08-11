084d79095d8c07d576d335d70dcd4931e38a7339
Björn Benouarets <70456959+bbenouarets@users.noreply.github.com>
2023-08-11 09:08:31 +0200

    Update

--- mail/GroupToDistrubtionList.ps1
+++ mail/GroupToDistrubtionList.ps1
@@ -1,18 +1,27 @@
 Import-Module AzureAD
 
+$LocalDomain = ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()).Name
+
 function Search-Permissions {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$LocalDomain,
-        [Parameter(Mandatory=$true)]
         [string]$Group
     )
-    # Check if user is member of the group in the local domain
-    $GroupMembers = Get-ADGroupMember -Identity $Group -Recursive | Select-Object -ExpandProperty SamAccountName
-    if ($GroupMembers -contains $env:USERNAME) {
-        Write-Host -ForegroundColor Green "User $($env:USERNAME) is member of group $($Group) in domain $($LocalDomain)!"
-    } else {
-        Write-Host -ForegroundColor Red "User $($env:USERNAME) is not member of group $($Group) in domain $($LocalDomain)!"
+    try {
+        if (Get-ADGroup -Identity $Group -ErrorAction SilentlyContinue) {
+            Write-Host -ForegroundColor Yellow "Group $($Group) found in domain $($LocalDomain)!"
+        } else {
+            Write-Error -Category ObjectNotFound -Message "Group $($Group) not found in domain $($LocalDomain)!"
+        }
+        $GroupMembers = Get-ADGroupMember -Identity $Group -Recursive | Select-Object -ExpandProperty SamAccountName
+        if ($GroupMembers -contains $env:USERNAME) {
+            Write-Host -ForegroundColor Green "User $($env:USERNAME) is member of group $($Group) in domain $($LocalDomain)!"
+        } else {
+            Write-Host -ForegroundColor Red "User $($env:USERNAME) is not member of group $($Group) in domain $($LocalDomain)!"
+        }
+    }
+    catch {
+        Write-Error -Category ObjectNotFound -Message "Group $($Group) not found in domain $($LocalDomain)!"
     }
 }
 
@@ -26,20 +35,19 @@ function Authenticate {
         $UserPrincipalName = $env:USERNAME + "@$Domain"
     } else {
         Write-Host -ForegroundColor Yellow "Using local domain..."
-        $LocalDomain = ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()).Name
         $UserPrincipalName = $env:USERNAME + "@$LocalDomain"
     }
     # Use the logged on user to authenticate to Azure AD
     Write-Host -ForegroundColor Yellow "Authenticating as $UserPrincipalName to Azure AD..."
     try {   
-        Connect-AzureAD -AccountId $UserPrincipalName -ErrorAction Stop
+        Connect-AzureAD -AccountId $UserPrincipalName -ErrorAction Stop | Out-Null
         Write-Host -ForegroundColor Green "Successfully authenticated to Azure AD!"
     }
     catch {
         Write-Error -Category ConnectionError -Message "Failed to authenticate to Azure AD!"
     }
     # Check if user is member of the group in the local domain
-    Search-Permissions -LocalDomain $LocalDomain -Group "Domain Admins"
+    Search-Permissions -Group "Domain Admins"
 }
 
 Authenticate -Domain "bytesentinel.io"
